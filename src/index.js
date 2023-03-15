80a8cddda97624fe140ce5bc1265790a978dd7c6
Kwame Cody <kwamecody@gmail.com>
2023-03-15 03:33:25 +0000

    home page  module

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ import { menu } from './modules/menu';
 import { contact } from './modules/contact';
 import './styles.css';
 
+homePage();
+
+export function createEl(el, className, src, text){
+    const element = document.createElement(`${el}`);
+    element.className = className;
+    element.src = src;
+    element.textContent = text;
+    return element;
+}
--- src/modules/home.js
+++ src/modules/home.js
@@ -1,6 +1,20 @@
-
-
+import { createEl } from "..";
 
 export function homePage(){
-    console.log("This is homepage");
+    const container = document.querySelector('.main');
+    container.replaceChildren();
+    
+    const homeDiv = createEl('div', 'tabcontent home', '', '');
+    const text1 = createEl('p', 'home-text one', '', 'Best Restaurant in the world!');
+    const text2 = createEl('p', 'home-text two', '', 'Since 1901');
+    const chefImg = createEl('img', 'chef-img', "../dist/img/chef.jpg", '');
+    chefImg.alt = "chef";
+    const text3 = createEl('p', 'home-text three', '', "Visit Us now");
+
+
+    container.appendChild(homeDiv);
+    homeDiv.appendChild(text1);
+    text1.insertAdjacentElement('afterend', text2);
+    text2.insertAdjacentElement('afterend', chefImg);
+    chefImg.insertAdjacentElement('afterend', text3);
 }
